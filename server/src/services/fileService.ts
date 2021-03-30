0e785dba44106590254eff5cfd09042b8abd3048
Denis Samborsky <dennyjuice@gmail.com>
2021-03-30 22:29:46 +0700

    [SERVER] fix delete file

--- server/src/services/fileService.ts
+++ server/src/services/fileService.ts
@@ -21,7 +21,7 @@ class FileService {
     if (file.type === 'dir') {
       fs.rmdirSync(filePath);
     } else {
-      fs.unlinkSync(path.resolve(filePath, file.name));
+      fs.unlinkSync(path.resolve(filePath));
     }
   }
 
